30225c6675afab9ba0bb2e7e37476c408066a3ff
Bengejd <bengejd@miamioh.edu>
2021-08-02 15:35:30 -0500

    Update descriptions for defaults

--- libs/ngx-cron-editor/src/CronOptions.ts
+++ libs/ngx-cron-editor/src/CronOptions.ts
@@ -1,33 +1,33 @@
 export interface CronOptions {
+  // Required
+  cronFlavor: string; // ex: quartz
+  defaultTime: string, // ex: 00:00:00
+
+  // Optional
   formInputClass?: string;
 
   formRadioClass?: string; // Default: cron-editor-radio-button frequency-group
   formCheckboxClass?: string; // Default: checkbox-margin
   formSelectClass?: string; // Default: form-control cron-editor-input
 
-  texts?: CronTextOptions;
-  reactive?: boolean;
+  texts?: CronTextOptions; // Default: {}
 
-  defaultTime?: string,
-
-  hideMinutesTab?: boolean;
-  hideHourlyTab?: boolean;
-  hideDailyTab?: boolean;
-  hideWeeklyTab?: boolean;
-  hideMonthlyTab?: boolean;
-  hideYearlyTab?: boolean;
-  hideAdvancedTab?: boolean;
+  hideMinutesTab?: boolean; // Default: false
+  hideHourlyTab?: boolean; // Default: false
+  hideDailyTab?: boolean; // Default: false
+  hideWeeklyTab?: boolean; // Default: false
+  hideMonthlyTab?: boolean; // Default: false
+  hideYearlyTab?: boolean; // Default: false
+  hideAdvancedTab?: boolean; // Default: false
   hideSpecificWeekDayTab?: boolean;
   hideSpecificMonthWeekTab?: boolean;
-  hideNearestWeekdayCheck?: boolean;
+  hideNearestWeekdayCheck?: boolean; // Default: false
 
-  use24HourTime?: boolean;
-  hideSeconds?: boolean;
+  use24HourTime?: boolean; // Default: true
+  hideSeconds?: boolean; // Default: false
 
-  removeSeconds?: boolean;
-  removeYears?: boolean;
-
-  cronFlavor: string;
+  removeSeconds?: boolean; // Default: true
+  removeYears?: boolean; // Default: true
 }
 
 interface CronTextOptions {
