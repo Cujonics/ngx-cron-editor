1866f92cd0e2ec56924f9673a3e26c79576dade6
Bengejd <bengejd@miamioh.edu>
2021-08-02 14:31:22 -0500

    Update app.component.ts

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -12,11 +12,6 @@ export class AppComponent implements OnInit {
   public cronExpression = '0 0 1/1 * *';
   public isCronDisabled = false;
   public cronOptions: CronOptions = {
-    formInputClass: 'form-control cron-editor-input',
-    formSelectClass: 'form-control cron-editor-select',
-    formRadioClass: 'cron-editor-radio',
-    formCheckboxClass: 'cron-editor-checkbox',
-
     texts: {},
 
     defaultTime: '00:00:00',
@@ -30,6 +25,7 @@ export class AppComponent implements OnInit {
     hideAdvancedTab: true,
     hideSpecificWeekDayTab: false,
     hideSpecificMonthWeekTab: false,
+    hideNearestWeekdayCheck: true,
 
     use24HourTime: true,
     hideSeconds: false,
